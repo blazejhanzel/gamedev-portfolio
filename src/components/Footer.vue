4eb23106db49a252aca6506b0ff894241f6471d8
Błażej Hanzel <29534847+blazejhanzel@users.noreply.github.com>
2023-04-02 10:43:19 +0200

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,52 +1,73 @@
 <template>
-  <div class="footer">
-    
-    <div class="left"><a href="https://github.com/schouffy/gamedev-portfolio" target="blank">Portfolio</a> by schouffy</div>
-    <div class="right">Reach me at <a href="mailto:my@email.com">my@email.com</a> or <router-link to="/contact">through here</router-link></div>
-  </div>
+    <div class="footer">
+        <div class="left"><a href="https://github.com/schouffy/gamedev-portfolio" target="blank">Portfolio</a> by schouffy
+        </div>
+        <div class="right">
+            Made with <span :style="{ color: heartColor }" title="Made in Silesia, Poland">{{ heart }}</span>
+            by <a href="https://github.com/blazejhanzel" target="_blank">Błażej Hanzel</a>, &copy; {{ currentYear }}
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "Footer"
+    name: "Footer",
+    data() {
+        return {
+            heart: "💙",
+            heartColor: "#007aff",
+        };
+    },
+    mounted() {
+        setInterval(() => {
+            this.heart = this.heart === "💙" ? "💛" : "💙";
+            this.heartColor = this.heart === "💙" ? "#007aff" : "#ffcc00";
+        }, 1000);
+    },
+    computed: {
+        currentYear() {
+            const currentYear = new Date().getFullYear();
+            return currentYear > 2023 ? `2023-${currentYear}` : "2023";
+        },
+    },
 });
 </script>
 
 <style scoped lang="less">
-
 @import '../css/variables.less';
 
 .footer {
-  background-color: @bodyBgColor;
-  width: 100%;
-  font-size: 0.8em;
-  opacity: 0.7;
-  padding-bottom: 30px;
+    background-color: @bodyBgColor;
+    width: 100%;
+    font-size: 0.8em;
+    opacity: 0.7;
+    padding-bottom: 30px;
 }
 
-.left, .right {
+.left,
+.right {
     padding-top: 10px;
     text-align: center;
-  }
+}
 
-@media only screen and (min-width: 620px){
+@media only screen and (min-width: 620px) {
+    .footer {
+        padding: 0;
+    }
 
-  .footer {
-    padding: 0;
-  }
+    .left,
+    .right {
+        padding: 20px;
+    }
 
-  .left, .right {
-    padding: 20px;
-  }
+    .left {
+        float: left;
+    }
 
-  .left {
-    float: left;
-  }
-
-  .right {
-    float:right;
-  }
+    .right {
+        float: right;
+    }
 }
 </style>
